1b649393f764844f30b701c9a037d765b1a92362
Bragi Bergþórsson <bragur@gmail.com>
2016-01-29 11:51:09 +0000

    Added Rect class

---
+++ js/Rect.js
@@ -0,0 +1,17 @@
+function Rect(position, size) {
+	this.position: position;
+	this.size: size;
+
+	this.isInRect = function(point) {
+		if (this.position.x < point.x && this.position.y < point.y) {
+			if (point.x - this.position.x < this.size.x && point.y - this.position.y < this.size.y) {
+				return true;
+			}
+		}
+		return false;
+	}
+	this.log = function() {
+		this.position.log();
+		this.size.log();
+	}
+};
