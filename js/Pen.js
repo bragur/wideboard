840a9f6499f910c1df1c7f8fcd4b8eb201abdf46
Baldur Tryggvason <ballit85@gmail.com>
2016-01-30 11:42:45 +0000

    Add Move functionality to Pen.js class

--- js/Pen.js
+++ js/Pen.js
@@ -3,6 +3,12 @@ var Pen = Shape.extend({
         this.path = new Array();
         this.lineWidth = lineWidth;
         this.base("Pen", position, color);
+        this.minX = this.position.x;
+        this.minY = this.position.y;
+        this.maxX = this.position.x;
+        this.maxY = this.position.y;
+        this.xOffset = this.position.x;
+        this.yOffset = this.position.y;
     },
 
     draw: function(canvas) {
@@ -20,20 +26,28 @@ var Pen = Shape.extend({
     },
 
     added: function(canvas) {
-        // for (var i = 0; i < this.path.length; i++) {
-        //     if (this.path[i].x < this.position.x) {
-        //         this.position.x = this.path[i].x;
-        //     }
-        //     if (this.path[i].x > this.size.x) {
-        //         this.size.x = this.path[i].x;
-        //     }
-        //     if (this.path[i].y < this.position.y) {
-        //         this.position.y = this.path[i].y;
-        //     }
-        //     if (this.path[i].y > this.size.y) {
-        //         this.size.y = this.path[i].y;
-        //     }
-        // }
+        for (var i = 0; i < this.path.length; i++) {
+            if (this.path[i].x < this.minX) {
+                this.minX = this.path[i].x;
+            }
+            if (this.path[i].x > this.maxX) {
+                this.maxX = this.path[i].x;
+            }
+            if (this.path[i].y < this.minY) {
+                this.minY = this.path[i].y;
+            }
+            if (this.path[i].y > this.maxY) {
+                this.maxY = this.path[i].y;
+            }
+        }
+
+        this.size.x = this.maxX - this.minX;
+        this.size.y = this.maxY - this.minY;
+        this.position.x = this.minX;
+        this.position.y = this.minY;
+
+        this.xOffset = this.position.x;
+        this.yOffset = this.position.y;
     },
 
     drawPath: function(canvas) {
@@ -41,15 +55,21 @@ var Pen = Shape.extend({
         canvas.beginPath();
         canvas.strokeStyle = this.color;
         canvas.lineWidth = this.lineWidth;
-        canvas.moveTo(this.path[0].x, this.path[0].y);
 
-        for (var i = 1; i < this.path.length - 1; i++) {
-            var c = (this.path[i].x + this.path[i + 1].x) / 2;
-            var d = (this.path[i].y + this.path[i + 1].y) / 2;
+        var xOff = this.position.x - this.xOffset;
+        var yOff = this.position.y - this.yOffset;
 
-            canvas.quadraticCurveTo(this.path[i].x, this.path[i].y, c, d);
+        if (this.path.length > 1) {
+            canvas.moveTo(this.path[1].x + xOff, this.path[1].y + yOff);
+
+            for (var i = 1; i < this.path.length - 1; i++) {
+                var c = (this.path[i].x + xOff + this.path[i + 1].x + xOff) / 2;
+                var d = (this.path[i].y + yOff + this.path[i + 1].y + yOff) / 2;
+
+                canvas.quadraticCurveTo(this.path[i].x + xOff, this.path[i].y + yOff, c, d);
+            }
+
+            canvas.stroke();
         }
-
-        canvas.stroke();
     },
 });
